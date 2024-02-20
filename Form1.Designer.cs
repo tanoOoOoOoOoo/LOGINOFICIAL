cbbee2d1d1f4c8c594a321a132fb2aaf82c6f5ed
Cayetano <cjimenez@itpfp.com>
2024-02-20 12:36:58 +0100

    cuartocommitañafoform1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace LOGINOFICIAL
+namespace LOGIN
 {
     partial class Form1
     {
@@ -29,13 +29,91 @@ namespace LOGINOFICIAL
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(113, 158);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(58, 17);
+            this.Usuario.TabIndex = 1;
+            this.Usuario.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(223, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Entrar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(516, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LOGINOFICIAL
+namespace LOGIN
 {
     public partial class Form1 : Form
     {
@@ -16,5 +16,41 @@ namespace LOGINOFICIAL
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            string Nombre = textBox1.Text;
+            string contraseña = textBox2.Text;
+
+            // Verificar si el usuario y la contraseña están rellenados
+            if (!string.IsNullOrEmpty(Nombre) && !string.IsNullOrEmpty(contraseña))
+            {
+                // Aquí puedes realizar la autenticación
+                // Puedes comparar con credenciales predefinidas o realizar una verificación en una base de datos, etc.
+
+                // Ejemplo de verificación simple
+                if (Nombre == "usuario" && contraseña == "contraseña")
+                {
+                    MessageBox.Show("¡Inicio de sesión exitoso!");
+                }
+                else
+                {
+                    MessageBox.Show("Credenciales incorrectas. Intenta de nuevo.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa tanto el usuario como la contraseña.");
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Aquí puedes agregar lógica para el registro de nuevos usuarios
+            // Puedes abrir otro formulario para el registro o realizar operaciones necesarias.
+
+            MessageBox.Show("¡Registro de nuevo usuario!");
+        }
     }
-}
+    }
+
